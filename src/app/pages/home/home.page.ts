40c46bbae5669841ca0007e76965a36f27868b88
Geekcode74 <creamfields44@gmail.com>
2019-09-19 18:06:36 -0500

    Router link using interfaces

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePage implements OnInit {
 
+  componentes: Componente[] = [
+    {
+      icon: 'american-football',
+      name: 'alert',
+      redirectTo: '/alert'
+    },
+    {
+      icon: 'appstore',
+      name: 'action sheet',
+      redirectTo: 'action-sheet'
+    }
+  ];
+
+
   constructor() { }
 
   ngOnInit() {
   }
 
+}
+
+interface Componente {
+  icon: string;
+  name: string;
+  redirectTo: string;
 }
